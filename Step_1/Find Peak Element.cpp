71d1063a456621da184d264cf90b4eceb4689fcc
AryanKashyap2812 <acad.aryankashyap2812@gmail.com>
2025-04-16 18:46:08 +0530

    changes

---
+++ Step_1/Find Peak Element.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        if (n == 1)
+            return 0;
+
+        if (nums[0] > nums[1])
+            return 0;
+
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+
+        int l = 1, h = n - 2;
+
+        while (l <= h)
+        {
+            int m = l + (h - l) / 2;
+
+            if (nums[m] > nums[m - 1] && nums[m] > nums[m + 1])
+            {
+                return m;
+            }
+
+            if (nums[m] > nums[m - 1])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                h = m - 1;
+            }
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int &i : v)
+    {
+        cin >> i;
+    }
+
+    Solution s;
+    cout << s.findPeakElement(v);
+}
