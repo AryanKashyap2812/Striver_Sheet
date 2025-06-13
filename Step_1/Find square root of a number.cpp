be7aec289ddbc0a26a70b9c16595e450519169e4
AryanKashyap2812 <acad.aryankashyap2812@gmail.com>
2025-06-13 22:14:57 +0530

    changes

---
+++ Step_1/Find square root of a number.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        int low = 0, mid, high = x, ans = 1;
+
+        while (low <= high)
+        {
+            mid = low + (high - low) / 2;
+            if ((mid * mid) <= x)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    Solution s;
+
+    cout << s.mySqrt(x);
+}
